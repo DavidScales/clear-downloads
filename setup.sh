afdcfe85cbf40fadde976ebee52949de6f354647
David Scales <davidsscales@gmail.com>
2019-09-12 10:56:01 -0700

    fix: pathing

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 echo "rm -rf /Users/demouser/Downloads/*" > /Users/demouser/clear-downloads.sh
 chmod a+x /Users/demouser/clear-downloads.sh
 
-cat startup.txt > ~/Library/LaunchAgents/com.user.loginscript.plist
+cat /Users/demouser/clear-downloads/startup.txt > ~/Library/LaunchAgents/com.user.loginscript.plist
 launchctl load ~/Library/LaunchAgents/com.user.loginscript.plist
